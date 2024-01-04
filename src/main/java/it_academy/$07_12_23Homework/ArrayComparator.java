e8e19ae39e6ec22771a08531bbe1cbcbaeb88a6f
Timon <Timon321.86@gmail.com>
2024-01-04 15:39:28 +0300

    Correct comparator.

--- src/main/java/it_academy/$07_12_23Homework/ArrayComparator.java
+++ src/main/java/it_academy/$07_12_23Homework/ArrayComparator.java
@@ -5,12 +5,15 @@ import java.util.Comparator;
 public class ArrayComparator implements Comparator<String[]> {
 	@Override
 	public int compare(String[] o1, String[] o2) {
-		for (int i = 0; i <(Math.min(o1.length,o2.length) ); i++) {
-			int comp=o1[i].compareTo(o2[i]);
-			if(comp!=0){
+		for (int i = 0; i < (Math.min(o1.length, o2.length)); i++) {
+			int comp = o1[i].compareTo(o2[i]);
+			if (comp != 0) {
 				return comp;
 			}
 		}
-	return 0;
+		if (o1.length != o2.length) {
+			return o1.length - o2.length;
+		}
+		return 0;
 	}
 }
