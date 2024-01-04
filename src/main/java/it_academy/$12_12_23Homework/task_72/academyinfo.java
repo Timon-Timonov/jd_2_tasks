5601169fa9e2ea4d33f3da8f200b55817d30c020
Timon <Timon321.86@gmail.com>
2024-01-04 17:50:55 +0300

    Tasks: 70, 71, 72.

---
+++ src/main/java/it_academy/$12_12_23Homework/task_72/academyinfo.java
@@ -0,0 +1,12 @@
+package it_academy.$12_12_23Homework.task_72;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface academyinfo {
+	public int year();
+}
