a1333dbec9218afbbd9103f25fbc1b42288eed0e
Timon <Timon321.86@gmail.com>
2024-01-04 15:29:16 +0300

    Initial commit.

---
+++ src/main/java/it_academy/$07_12_23Homework/ArrayComparator.java
@@ -0,0 +1,16 @@
+package it_academy.$07_12_23Homework;
+
+import java.util.Comparator;
+
+public class ArrayComparator implements Comparator<String[]> {
+	@Override
+	public int compare(String[] o1, String[] o2) {
+		for (int i = 0; i <(Math.min(o1.length,o2.length) ); i++) {
+			int comp=o1[i].compareTo(o2[i]);
+			if(comp!=0){
+				return comp;
+			}
+		}
+	return 0;
+	}
+}
