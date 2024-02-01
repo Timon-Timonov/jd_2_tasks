13660a052e8f84262183a6c5db043044e7b756e5
Timon <Timon321.86@gmail.com>
2024-02-01 12:15:09 +0300

    App is working.

---
+++ src/main/java/it_academy/lesson25_01_24Homework/dao/PersonDao.java
@@ -0,0 +1,11 @@
+package it_academy.lesson25_01_24Homework.dao;
+
+import it_academy.lesson25_01_24Homework.dto.Person;
+
+import java.util.List;
+
+public interface PersonDao {
+	Person save(Person person);
+
+	List<Person> find(String columnNameFind, Number paramExcl, String columnNameOrderAsc);
+}
---
+++ src/main/java/it_academy/lesson25_01_24Homework/dao/PersonDaoImpl.java
@@ -0,0 +1,45 @@
+package it_academy.lesson25_01_24Homework.dao;
+
+import it_academy.lesson25_01_24Homework.HibernateUtils.HibernateUtils;
+import it_academy.lesson25_01_24Homework.dto.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class PersonDaoImpl implements PersonDao {
+	@Override
+	public Person save(Person person) {
+		EntityManager em = HibernateUtils.getEntityManager();
+		em.getTransaction().begin();
+		em.persist(person);
+		em.getTransaction().commit();
+		Person person1 = em.find(Person.class, person.getId());
+		em.close();
+		return person1;
+	}
+
+	@Override
+	public List<Person> find(String columnNameFind, Number paramExcl, String columnNameOrderAsc) {
+		List<Person> personList;
+		EntityManager em = HibernateUtils.getEntityManager();
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery<Person> criteriaQuery = criteriaBuilder.createQuery(Person.class);
+		Root<Person> root = criteriaQuery.from(Person.class);
+		Order order = criteriaBuilder.asc(root.get(columnNameOrderAsc));
+		criteriaQuery
+				.select(root)
+				.where(criteriaBuilder.gt(root.get(columnNameFind), paramExcl))
+				.orderBy(order);
+
+
+		Query query = em.createQuery(criteriaQuery);
+		personList = query.getResultList();
+		em.close();
+		return personList;
+	}
+}
