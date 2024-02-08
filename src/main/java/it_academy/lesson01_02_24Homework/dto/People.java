7143b61689203387954ed8901d3b4d9fba5e52e5
Timon <Timon321.86@gmail.com>
2024-02-08 11:15:50 +0300

    Task 9.3 Many to many.

--- src/main/java/it_academy/lesson01_02_24Homework/dto/People.java
+++ src/main/java/it_academy/lesson01_02_24Homework/dto/People.java
@@ -3,6 +3,8 @@ package it_academy.lesson01_02_24Homework.dto;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Builder
 @NoArgsConstructor
@@ -12,20 +14,26 @@ import javax.persistence.*;
 @EqualsAndHashCode
 @ToString
 @Entity
-@Table(name="people")
+@Table(name = "people")
 public class People {
 
 	@Id
-	@Column(name="id")
+	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	@Column(name="name")
+	@Column(name = "name")
 	private String name;
 
-	@Column(name="surname")
+	@Column(name = "surname")
 	private String surname;
 
-	@Column(name="age")
+	@Column(name = "age")
 	private int age;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "people_addresses",
+			joinColumns = {@JoinColumn(name = "people_id")},
+			inverseJoinColumns = {@JoinColumn(name = "address_id")})
+	private Set<Address> addresses = new HashSet<>();
 }
